376796d0e0b915e4cdadda8e26e731b2a6d87918
BodickLee <liushaobo@liushaobodeMacBook-Air.local>
2024-07-30 18:02:12 +0800

    first commit

---
+++ build-a-sudoku-solver.py
@@ -0,0 +1,19 @@
+puzzle = [
+  [1, 1, 2, 0, 0, 8, 0, 0, 0],
+  [0, 0, 0, 0, 0, 3, 7, 6, 2],
+  [4, 3, 0, 0, 0, 0, 8, 0, 0],
+  [0, 5, 0, 0, 3, 0, 0, 9, 0],
+  [0, 4, 0, 0, 0, 0, 0, 2, 6],
+  [0, 0, 0, 4, 6, 7, 0, 0, 0],
+  [0, 8, 6, 7, 0, 4, 0, 0, 0],
+  [0, 0, 0, 5, 1, 9, 0, 0, 8],
+  [1, 7, 0, 0, 0, 6, 0, 0, 5]
+]
+
+# Flatten the puzzle list
+flat_puzzle = [item for sublist in puzzle for item in sublist]
+
+# Find the first occurrence of 0
+first_zero_index = flat_puzzle.index(0)
+
+print(first_zero_index)
